c3b7a4000b5f90ed4af990408f7b354496a9e9e5
Mario López <mlopezramonet@gmail.com>
2023-09-28 10:01:44 -0700

    Agregar código demo componente Vue de Números Favoritos

---
+++ numFavoritos.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Mis números favoritos</h2>
+    <input v-model="numero" placeholder="Agregar número" />
+    <button @click="addNumero" :disabled="!validarNum">Agregar</button>
+    <ul>
+      <li v-for="(numero, index) in numFavoritos" :key="index">{{ numero }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero: "",
+      numFavoritos: [],
+    };
+  },
+  computed: {
+    validarNum() {
+      return this.esNumExistente && this.numero.length == 0;
+    },
+    esNumExistente() {
+      return this.numFavoritos.includes(parseInt(this.numero));
+    },
+  },
+  methods: {
+    addNumero() {
+      if (!this.esNumExistente) {
+        this.numFavoritos.push(parseInt(this.numero));
+        this.numero = "";
+      }
+    },
+  },
+};
+</script>
